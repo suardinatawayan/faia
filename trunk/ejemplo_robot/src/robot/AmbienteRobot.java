75ebd18e15124c6972736f627bf65c94e47d9d7d
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2008-05-20 15:24:36 +0000

    Subo el ejemplo del robot

---
+++ trunk/ejemplo_robot/src/robot/AmbienteRobot.java
@@ -0,0 +1,24 @@
+package robot;
+
+import frsf.cidisi.faia.agent.Agent;
+import frsf.cidisi.faia.agent.Perception;
+import frsf.cidisi.faia.environment.Environment;
+
+public class AmbienteRobot extends Environment{
+
+	public AmbienteRobot(){
+		// Se instancia el estado del ambiente.-
+		this.environmentState = new EstadoAmbiente();
+	}
+	
+	public Perception getPercept(Agent agent){
+		// Retorna la nueva percepción creada.-
+		return null;
+	}
+	
+	public String toString(){
+		
+//		return environmentState.toString();
+		return "";
+	}
+}
---
+++ trunk/ejemplo_robot/src/robot/IrA.java
@@ -0,0 +1,56 @@
+package robot;
+
+import java.util.ArrayList;
+
+import frsf.cidisi.faia.agent.problem.Action;
+import frsf.cidisi.faia.state.AgentState;
+import frsf.cidisi.faia.state.EnvironmentState;
+
+public class IrA extends Action {
+
+	/**
+	 * 
+	 * @param s: Es el estado del agente en un determinado nodo del árbol de búsqueda.-
+	 */
+	public AgentState execute(AgentState s){
+		EstadoRobot estR = (EstadoRobot)s;
+		
+		if (estR.getPosicionesVisitadas().contains(EstadoRobot.A))
+			return null;
+		
+		ArrayList<String> sucesores = new ArrayList<String>();
+		sucesores.addAll(estR.getSucesores());
+		if (sucesores!=null){
+			int index = sucesores.indexOf(EstadoRobot.A);
+			if (index>=0){
+				estR.setPosicion(EstadoRobot.A);
+				
+				return estR;
+			}
+			
+		}
+		
+		return null;
+	}
+
+	/**
+	 * Permite actualizar el estado real del agente y del ambiente.-
+	 * 
+	 * @param ast: Es el estado del agente a ser actualizado.-
+	 * @param est: Es el estado del ambiente a ser actualizado.-
+	 */
+	public EnvironmentState execute(AgentState ast, EnvironmentState est){
+		this.execute(ast);
+
+		return null;
+	}
+	
+	public Double getCost(){
+		return new Double(0);
+	}
+
+	public String toString() {
+		return "IrA";
+	}
+
+}
