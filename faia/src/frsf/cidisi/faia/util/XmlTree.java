06068574d5bd542b52e8e9ce4518dbee7e99bcd6
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2008-06-02 21:49:03 +0000

    - Agrego un archivo FileOperations para realizar, en este caso, la copia fácil de archivos. Se pueden agregar acá otras operaciones que se vayan a usar. - Muevo la carpeta 'pdflatex' al framework faia, y desde éste es copiado antes de compilar. OJO: hay un problema acá, y es que se supone que faia se encuentra en la carpeta '../faia'. Habría que buscar una forma de que esto no esté hardcodeado. - LatexOutput y XmlTree se fijan si existen los directorios de salida. Sino los crean.

--- faia/src/frsf/cidisi/faia/util/XmlTree.java
+++ faia/src/frsf/cidisi/faia/util/XmlTree.java
@@ -1,17 +1,25 @@
 package frsf.cidisi.faia.util;
 
+import java.io.File;
+
 import frsf.cidisi.faia.solver.search.NTree;
 
 public class XmlTree {
 	
 	private static int fileIdx = 0;
+	private static final String searchTreesDir = "searchTrees/";
 	
     public static void printFile(NTree tree) {
     	
     	PrintOut print = null;
 		
 		try{
-	    	print = new PrintOut("searchTrees/"+fileIdx + ".xml");
+			// Si la carpeta que necesito no existe, la creo.
+			File f = new File(searchTreesDir);
+			if (!f.exists())
+				f.mkdir();
+			
+	    	print = new PrintOut(searchTreesDir + fileIdx + ".xml");
 			fileIdx = fileIdx + 1;
 		
 			print.write(tree.toXml());
