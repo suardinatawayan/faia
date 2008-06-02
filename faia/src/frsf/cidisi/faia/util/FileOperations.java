980a27a65299c92788c2aa658eb8e7a5d43e2e5d
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2008-06-02 21:50:06 +0000

    Me olvidé del FileOperations :)

---
+++ faia/src/frsf/cidisi/faia/util/FileOperations.java
@@ -0,0 +1,23 @@
+package frsf.cidisi.faia.util;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileOperations {
+	public static void CopyFile(String source, String dest) throws IOException {
+		File inputFile = new File(source);
+	    File outputFile = new File(dest);
+
+	    FileReader in = new FileReader(inputFile);
+	    FileWriter out = new FileWriter(outputFile);
+	    int c;
+
+	    while ((c = in.read()) != -1)
+	      out.write(c);
+
+	    in.close();
+	    out.close();
+	}
+}
