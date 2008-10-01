1aa77b09d404b33e511f54c81f731be3c6fe6b27
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2008-10-01 22:32:35 +0000

    Comentario para no olvidarme en la acción Comer

--- trunk/ejemplos_calculo/pacman_logico/src/agente/acciones/Comer.java
+++ trunk/ejemplos_calculo/pacman_logico/src/agente/acciones/Comer.java
@@ -27,11 +27,24 @@ public class Comer extends CalculusAction {
         return null;
     }
 
+    /**
+     * Si bien el uso de éste método no es muy importante cuando se crea un
+     * agente basado en búsqueda, sí es vital establecer correctamente el nombre
+     * de la acción cuando se crea un agente basado en cálculo situacional, como
+     * en este caso.
+     * Mirar el código del método CalculusAction.toLogicName(). Lo que hace ese
+     * método es pasar el resultado de éste a minúsculas, y supone que en el
+     * archivo prolog se está utilizando ese string (en minúsculas) para indicar
+     * la acción.
+     * 
+     * En este ejemplo, en el archivo 'base_conocimiento.pl' (al que llamamos
+     * archivo prolog) se utiliza el string 'comer' para indicar este acción.
+     * Por lo tanto es correcto utilizar aquí tanto 'Comer', como 'COMER', pero
+     * no 'AccionComer', ya que se transformaría a 'accioncomer' y esa no es
+     * la representación correcta de la acción Comer en el archivo prolog.
+     */
     @Override
     public String toString() {
-        /* El nombre de la acción se pasará a minúsculas para trabajar
-         * con la base de conocimiento, por lo tanto se debe utilizar el mismo
-         * nombre aquí */
         return "Comer";
     }
 }
