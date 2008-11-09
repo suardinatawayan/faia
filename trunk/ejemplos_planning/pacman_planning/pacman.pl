78671b8584f2ea4b3076d63c15acdecba1d4262b
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2008-11-09 02:30:54 +0000

    Ultimas modificaciones de prueba

--- trunk/ejemplos_planning/pacman_planning/pacman.pl
+++ trunk/ejemplos_planning/pacman_planning/pacman.pl
@@ -9,11 +9,11 @@
 %
 
 % avanzar(Pos1,Pos2)
-preconditions( avanzar(Pos1,Pos2), [en(Pos1), adyacente(Pos1,Pos2)] ).
-%preconditions( avanzar(Pos1,Pos2), [en(Pos1), adyacente(Pos1,Pos2), celdaNoVisitada(Pos2)] ).
+%preconditions( avanzar(Pos1,Pos2), [en(Pos1), adyacente(Pos1,Pos2)] ).
+preconditions( avanzar(Pos1,Pos2), [en(Pos1), adyacente(Pos1,Pos2), celdaNoVisitada(Pos2)] ).
 achieves( avanzar(Pos1,Pos2), en(Pos2) ).
 deletes( avanzar(Pos1,Pos2), en(Pos1) ).
-%deletes( avanzar(Pos1,Pos2), celdaNoVisitada(Pos2) ).
+deletes( avanzar(Pos1,Pos2), celdaNoVisitada(Pos2) ).
 
 % descubrir(Pos)
 preconditions( descubrir(Pos), [en(Pos), desconocido(Pos)] ).
@@ -39,108 +39,26 @@ primitive( adyacente(_,_) ).
 primitive( enemigo(_) ).
 primitive( comida(_) ).
 primitive( vacio(_) ).
-%primitive( celdaNoVisitada(_) ).
 primitive( desconocido(_) ).
 primitive( descubre(_) ).
 primitive( pelea(_) ).
 primitive( come(_) ).
+primitive( celdaNoVisitada(_) ).
+
+%
+% tamaño_mundo(N).
+%
+% Indica qué tamaño tendrá el mundo, que será de N x N.
+%
+
+tamaño_mundo(3).
 
 
-
-% Adyacencia
-% 0   1   2   3
-% 4   5   6   7
-% 8   9   10  11
-% 12  13  14  15
-
-/*
-holds(adyacente(10,6), init).
-holds(adyacente(10,11), init).
-holds(adyacente(10,14), init).
-holds(adyacente(10,9), init).
-
-holds(adyacente(11,7), init).
-holds(adyacente(11,8), init).
-holds(adyacente(11,15), init).
-holds(adyacente(11,10), init).
-
-holds(adyacente(12,8), init).
-holds(adyacente(12,13), init).
-holds(adyacente(12,0), init).
-holds(adyacente(12,15), init).
-
-holds(adyacente(13,9), init).
-holds(adyacente(13,14), init).
-holds(adyacente(13,1), init).
-holds(adyacente(13,12), init).
-
-holds(adyacente(14,10), init).
-holds(adyacente(14,15), init).
-holds(adyacente(14,2), init).
-holds(adyacente(14,13), init).
-
-holds(adyacente(15,11), init).
-holds(adyacente(15,12), init).
-holds(adyacente(15,3), init).
-holds(adyacente(15,14), init).
-
-holds(adyacente(0,12), init).
-holds(adyacente(0,1), init).
-holds(adyacente(0,4), init).
-holds(adyacente(0,3), init).
-
-holds(adyacente(1,13), init).
-holds(adyacente(1,2), init).
-holds(adyacente(1,5), init).
-holds(adyacente(1,0), init).
-
-holds(adyacente(2,14), init).
-holds(adyacente(2,3), init).
-holds(adyacente(2,6), init).
-holds(adyacente(2,1), init).
-
-% 0   1   2   3
-% 4   5   6   7
-% 8   9   10  11
-% 12  13  14  15
-
-holds(adyacente(3,15), init).
-holds(adyacente(3,0), init).
-holds(adyacente(3,7), init).
-holds(adyacente(3,2), init).
-
-holds(adyacente(4,0), init).
-holds(adyacente(4,5), init).
-holds(adyacente(4,8), init).
-holds(adyacente(4,7), init).
-
-holds(adyacente(5,6), init).
-holds(adyacente(5,9), init).
-holds(adyacente(5,4), init).
-holds(adyacente(5,1), init).
-
-holds(adyacente(6,2), init).
-holds(adyacente(6,7), init).
-holds(adyacente(6,10), init).
-holds(adyacente(6,5), init).
-
-holds(adyacente(7,3), init).
-holds(adyacente(7,4), init).
-holds(adyacente(7,11), init).
-holds(adyacente(7,6), init).
-
-holds(adyacente(8,4), init).
-holds(adyacente(8,9), init).
-holds(adyacente(8,12), init).
-holds(adyacente(8,11), init).
-
-holds(adyacente(9,5), init).
-holds(adyacente(9,10), init).
-holds(adyacente(9,13), init).
-holds(adyacente(9,8), init).
-*/
-
-tamaño_mundo(2).
+%
+% generarPosicion(N).
+%
+% Da como solución todas las posiciones posibles del mundo.
+%
 
 generarPosicion(0).
 generarPosicion(N) :-
@@ -155,35 +73,41 @@ generarPosicionAux(Sig,Num) :-
 	generarPosicionAux(Sig, Num + 1).
 
 
-celdaCorrecta(Pos1, Pos2) :-
-	generarPosicion(Pos1),
-	generarPosicion(Pos2).
+%
+% Adyacencia de las celdas
+%
 
 holds(adyacente(Pos1, Pos2), init) :-
-	celdaCorrecta(Pos1,Pos2),
+	generarPosicion(Pos1),
+	generarPosicion(Pos2),
 	tamaño_mundo(T),
 	M is Pos1 mod T, M =\= T - 1,
 	Pos2 is Pos1 + 1.
 
 holds(adyacente(Pos1, Pos2), init) :-
-	celdaCorrecta(Pos1,Pos2),
+	generarPosicion(Pos1),
+	generarPosicion(Pos2),
 	tamaño_mundo(T),
 	M is Pos1 mod T, M =\= 0,
 	Pos2 is Pos1 - 1.
 
 holds(adyacente(Pos1, Pos2), init) :-
-	celdaCorrecta(Pos1,Pos2),
+	generarPosicion(Pos1),
+	generarPosicion(Pos2),
 	tamaño_mundo(T),
 	Pos1 < T * (T - 1),
 	Pos2 is Pos1 + T.
 
 holds(adyacente(Pos1, Pos2), init) :-
-	celdaCorrecta(Pos1,Pos2),
+	generarPosicion(Pos1),
+	generarPosicion(Pos2),
 	tamaño_mundo(T),
 	Pos1 >= T,
 	Pos2 is Pos1 - T.
-	
 
+
+% Ejemplo de un mundo 4 x 4
+%
 % 0   1   2   3
 % 4   5   6   7
 % 8   9   10  11
@@ -192,34 +116,12 @@ holds(adyacente(Pos1, Pos2), init) :-
 % ESTADO INICIAL
 
 holds(en(0), init).
-holds(comida(1), init).
 holds(comida(0), init).
+holds(comida(1), init).
 holds(vacio(2), init).
-holds(enemigo(3), init).
-%holds(comida(4), init).
-%holds(comida(3), init).
 %holds(comida(12), init).
+%holds(enemigo(15), init).
 
-%holds(desconocido(3), init).
-
-/*
-holds(celdaNoVisitada(0), init).
-holds(celdaNoVisitada(1), init).
-holds(celdaNoVisitada(2), init).
-holds(celdaNoVisitada(3), init).
-holds(celdaNoVisitada(4), init).
-holds(celdaNoVisitada(5), init).
-holds(celdaNoVisitada(6), init).
-holds(celdaNoVisitada(7), init).
-holds(celdaNoVisitada(8), init).
-holds(celdaNoVisitada(9), init).
-holds(celdaNoVisitada(10), init).
-holds(celdaNoVisitada(11), init).
-holds(celdaNoVisitada(12), init).
-holds(celdaNoVisitada(13), init).
-holds(celdaNoVisitada(14), init).
-holds(celdaNoVisitada(15), init).
-*/
 
 % Si no hay comida ni enemigo ni está vacía la celda, entonces
 % es desconocida.
@@ -304,7 +206,7 @@ debug([Init,Accion|As]) :-
 
 resolver(Init,Accion,S) :-
 	armarObjetivos(Objetivos),
-	solve(Objetivos,P,10),
+	solve(Objetivos,P,100),
 	seq(P,[Init,Accion|S]),!.
 
 
@@ -313,7 +215,7 @@ resolver(Init,Accion,S) :-
 %
 % X es alguna acción. Ejecuta la acción efectivamente, para que así al
 % correr otra vez 'obtenerAccion' se realice el paso siguiente. Quita
-% o agrega holds(...
+% o agrega holds(...)
 %
 
 ejecutarAccion(avanzar(Pos1,Pos2)) :-
