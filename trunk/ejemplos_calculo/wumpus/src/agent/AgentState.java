9c87fd2babde64aca933d2a69c8e6d5c08a15888
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2009-01-26 20:43:50 +0000

    Ahora si, subo la estructura (este Eclipse...)

---
+++ trunk/ejemplos_calculo/wumpus/src/agent/AgentState.java
@@ -0,0 +1,51 @@
+package agent;
+
+import frsf.cidisi.faia.agent.Perception;
+import frsf.cidisi.faia.exceptions.PrologConnectorException;
+import frsf.cidisi.faia.solver.ActionFactory;
+import frsf.cidisi.faia.solver.situationcalculus.KnowledgeBase;
+
+public class AgentState extends KnowledgeBase {
+
+	public AgentState() throws PrologConnectorException {
+		super("wumpus_world.pl");
+		
+		this.initState();
+	}
+	
+	@Override
+	public ActionFactory getActionFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBestActionPredicate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getExecutedActionPredicate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateState(Perception p) {
+		this.tell(p);
+	}
+
+	@Override
+	public void initState() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ trunk/ejemplos_calculo/wumpus/src/agent/WumpusWorldEnvironment.java
@@ -0,0 +1,15 @@
+package agent;
+
+import frsf.cidisi.faia.agent.Agent;
+import frsf.cidisi.faia.agent.Perception;
+import frsf.cidisi.faia.environment.Environment;
+
+public class WumpusWorldEnvironment extends Environment {
+
+	@Override
+	public Perception getPercept(Agent agent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
