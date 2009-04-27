f3f445bb51b074f342c6a2f03fb2ed3ff55f9a7d
jorgemarceloroa <jorgemarceloroa@105137fb-d23e-0410-9ed3-77b8c0201c27>
2009-04-27 17:35:57 +0000

    Probando la notificacion de google cuando se hace commit

--- examples/src/frsf/cidisi/faia/examples/search/pacman/MetaPacman.java
+++ examples/src/frsf/cidisi/faia/examples/search/pacman/MetaPacman.java
@@ -4,7 +4,7 @@ import frsf.cidisi.faia.agent.searchbased.GoalTest;
 import frsf.cidisi.faia.state.AgentState;
 
 /**
- * Esta clase se utiliza para definir una función que, recibido
+ * Esta clase se utiliza para definir una función que, recibido 
  * un objeto estado del agente, verifique si se ha llegado al objetivo o no.
  * Se utiliza en el proceso interno de búsqueda del agente y también en
  * el simulador, para saber cuándo detenerse.
