6337bf06f2fbe064e4e90f54a77f7084ac1b87a1
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2009-07-25 19:29:20 +0000

    - También actualizo en 'examples' los archivos de Netbeans. - La clase SimpleWaterTankEnvironmentState hereda ahora de MatlabEnvironmentState. - Subo un simulador personalizado que utiliza el ejemplo: TimeBasedSimulator. Hereda de ReactiveBasedAgentSimulator y sobreescribe el método 'isComplete' fijandose además si se ha llegado a un tiempo especificado.

--- trunk/examples/src/frsf/cidisi/faia/examples/matlab/simple_water_tank/SimpleWaterTankEnvironmentState.java
+++ trunk/examples/src/frsf/cidisi/faia/examples/matlab/simple_water_tank/SimpleWaterTankEnvironmentState.java
@@ -1,34 +1,31 @@
 package frsf.cidisi.faia.examples.matlab.simple_water_tank;
 
 import frsf.cidisi.faia.state.EnvironmentState;
+import frsf.cidisi.faia.state.MatlabEnvironmentState;
 
-public class SimpleWaterTankEnvironmentState extends EnvironmentState {
+public class SimpleWaterTankEnvironmentState extends MatlabEnvironmentState {
 
 	private double tankHeight;
-	
 	private double volumeFlow;
-
-	private int startTime;
-	private int endTime;
-	private int step;
 	
 	public SimpleWaterTankEnvironmentState() {
+		// Start time = 0
+		// Step = 3
+		super(0, 3);
+		
 		this.initState();
 	}
 	
 	@Override
 	public void initState() {
 		this.tankHeight = 5.0;
-		this.startTime = 0;
-		this.step = 3;
-		this.endTime = this.startTime + this.step;
 	}
 
 	@Override
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 		
-		sb.append("\nTimes: " + this.startTime + " -> " + this.endTime + "\n");
+		sb.append("\nTime: " + this.getStartTime() + "\n");
 		sb.append("Tank height: " + this.tankHeight + "\n");
 		sb.append("Volume flow: " + this.volumeFlow + "\n");
 		
@@ -49,25 +46,5 @@ public class SimpleWaterTankEnvironmentState extends EnvironmentState {
 	
 	public void setVolumeFlow(double volumeFlow) {
 		this.volumeFlow = volumeFlow;
-	}
-
-	public int getStartTime() {
-		return startTime;
-	}
-	
-	public void setStartTime(int startTime) {
-		this.startTime = startTime;
-	}
-
-	public int getEndTime() {
-		return endTime;
-	}
-	
-	public void setEndTime(int endTime) {
-		this.endTime = endTime;
-	}
-
-	public int getStep() {
-		return step;
 	}
 }
