ad62fe86e3024251d8515f848d855f8428342356
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2009-08-15 14:41:12 +0000

    Subo el ejemplo del wumpus funcional (falta pulir algunas cositas, como código comentado, etc, pero funciona perfecto).

---
+++ trunk/unstable_examples/wumpus/src/agent/actions/Forward.java
@@ -0,0 +1,19 @@
+package agent.actions;
+
+import frsf.cidisi.faia.agent.situationcalculus.SituationCalculusAction;
+import frsf.cidisi.faia.state.AgentState;
+import frsf.cidisi.faia.state.EnvironmentState;
+
+public class Forward extends SituationCalculusAction {
+
+	@Override
+	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
+		return est;
+	}
+
+	@Override
+	public String toString() {
+		return "forward";
+	}
+
+}
