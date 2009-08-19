7517cb462f3f85cb83a93458bfe9cde69ff84309
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2009-08-19 23:42:20 +0000

    El simulador de planning utiliza el nuevo diseño de simuladores (no estaba actualizado).

--- faia/src/frsf/cidisi/faia/simulator/PlanningBasedAgentSimulator.java
+++ faia/src/frsf/cidisi/faia/simulator/PlanningBasedAgentSimulator.java
@@ -22,8 +22,6 @@ import java.util.Vector;
 
 import frsf.cidisi.faia.agent.Action;
 import frsf.cidisi.faia.agent.Agent;
-import frsf.cidisi.faia.agent.Perception;
-import frsf.cidisi.faia.agent.planning.PlanningBasedAgent;
 import frsf.cidisi.faia.environment.Environment;
 import frsf.cidisi.faia.agent.NoAction;
 
@@ -35,58 +33,6 @@ public class PlanningBasedAgentSimulator extends GoalBasedAgentSimulator {
 
     public PlanningBasedAgentSimulator(Environment environment, Agent agent) {
         this(environment, new Vector<Agent>(Arrays.asList(agent)));
-    }
-
-    @Override
-    public void start() {
-        Perception perception;
-        Action action;
-        PlanningBasedAgent agent;
-
-        //TODO: Aca hay que tener en cuenta que podría haber más de un agente
-        // por ahora el framework solo es monoagente :)
-        agent = (PlanningBasedAgent) this.getAgents().firstElement();
-
-        do {
-
-            System.out.println("--------------------------------------");
-            System.out.println("--- Planning Based Agent Simulator ---");
-            System.out.println("--------------------------------------");
-
-            // We make the perception and send it to the agent.
-            perception = this.getPercept(agent);
-            agent.see(perception);
-
-            System.out.println("Agent State: " + agent.getAgentState());
-            System.out.println();
-            System.out.println("Environment: " + environment);
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-
-            // Ask agent for an action
-            action = agent.selectAction();
-
-            System.out.println("Action: " + action);
-
-            /* Check if agent has reached the goal or not, or if we must
-             * go on */
-            if (action != null && !(action instanceof NoAction)) {
-                /* If the action is not a NoAction instance, then we update
-                 * the real world on the simulator. */
-                this.updateState(action);
-            }
-        } while (!(action instanceof NoAction || action == null));
-
-        if (action instanceof NoAction) {
-            // If there is no action, then the agent has reached the goal.
-            System.out.println("Agent has reached the goal!");
-        } else {
-            // If action is null, then there was an error.
-            System.out.println("ERROR: There is not solution for this problem. You should check the operators.");
-        }
-
-        // Leave a blank line
-        System.out.println();
     }
 
     @Override
