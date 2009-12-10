9fc9041ad574dfbeef1f5fde20c6e19e96102dcd
miltondp <miltondp@105137fb-d23e-0410-9ed3-77b8c0201c27>
2009-12-10 23:58:42 +0000

    Corregido el archivo prolog del ejemplo del Pacman (cálculo situacional). También subo unos arreglos menores en el archivo prolog del Wumpus.

--- trunk/examples/wumpus_world.pl
+++ trunk/examples/wumpus_world.pl
@@ -1,4 +1,167 @@
-:- dynamic actualSituation/1, bump/2, glitter/2, wumpusKilled/1, action/2, stench/2, breeze/2, perception/2, at/3, position/2, orientation/2, holding/2.
+ %
+ % Copyright 2007-2009 Georgina Stegmayer, Milagros Gutiérrez, Jorge Roa,
+ % y Milton Pividori.
+ % 
+ % This program is free software: you can redistribute it and/or modify
+ % it under the terms of the GNU General Public License as published by
+ % the Free Software Foundation, either version 3 of the License, or
+ % (at your option) any later version.
+ % 
+ % This program is distributed in the hope that it will be useful,
+ % but WITHOUT ANY WARRANTY; without even the implied warranty of
+ % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ % GNU General Public License for more details.
+ % 
+ % You should have received a copy of the GNU General Public License
+ % along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ %
+
+% The code below is neccessary for the framework to modify these sentences.
+
+:- dynamic actualSituation/1, bump/2, glitter/2, wumpusKilled/1, action/2, stench/2,
+breeze/2, perception/2, at/3, position/2, orientation/2, holding/2.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Diagnostic rules                                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+stench(P,S) :-
+	perception([stench,_,_,_,_]),
+	actualSituation(S),
+	position(P,S).
+
+breeze(P,S) :-
+	perception([_,breeze,_,_,_]),
+	actualSituation(S),
+	position(P,S).
+
+glitter(P,S) :-
+	perception([_,_,glitter,_,_]),
+	actualSituation(S),
+	position(P,S).
+
+bump(S) :-
+	actualSituation(S),
+	perception([_,_,_,bump,_]).
+
+wumpusKilled(S) :-
+	perception([_,_,_,_,wumpusScream]),
+	actualSituation(S).
+
+at(nothing,P,S) :-
+	perception([nothing,nothing,nothing,nothing,nothing]),
+	actualSituation(S),
+	position(P,S).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Causal rules                                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% holding(O,S) : where O is an object holded by the agent, and S is a
+% situation.
+
+% action(A,S) : where A is an action taken by the agent at situation S.
+
+% safe(P,S) : P is a location which the agent knows it's safe to move on on
+% situation S.
+safe(P,S) :-
+	at(nothing,P,S).
+
+safe(Pa,S) :-
+	at(nothing,P,S),
+	adjacent(P,Pa),
+	not(at(wall,Pa,S)).
+
+% unknown(P,S) : P is a unknown location where for the agent at situation S.
+unknown([R,C],S) :-
+	R < 4, C < 4,
+	not(stench([R,C],S)),
+	not(breeze([R,C],S)),
+	not(glitter([R,C],S)),
+	not(at(Sm,[R,C],S)).
+
+% belief(O,P,S) : where O is an object that the agent *believes* is at location
+% P in situaion S.
+belief(wumpus,Pa,S) :-
+	stench(P,S),
+	adjacent(P,Pa),
+	not(safe(Pa,S)).
+
+belief(pit,Pa,S) :-
+	breeze(P,S),
+	adjacent(P,Pa),
+	not(safe(Pa,S)).
+
+% at(O,P,S) : where O is an object, P is a location, and S is a situation.
+at(wall,Pa,S) :-
+	bump(S),
+	position(P,S),
+	orientation(O,S),
+	adjacent2(O,P,Pa).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Successor state axioms                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% When position doesn't change.
+est(S1):- S1 > 0,S is S1-1,action(A,S),A\=forward,position(P,S),asserta(position(P,S1)).
+est(S1):- S1 > 0,S is S1-1,action(forward,S),position(P,S),orientation(O,S),not(adjacent2(O,P,_)),asserta(position(P,S1)).
+
+% When orientation doesn't change.
+est(S1):- S1 > 0,S is S1-1,action(A,S),A\=turnright,A\=turnleft,orientation(O,S),asserta(orientation(O,S1)).
+
+% Position and orientation change.
+est(S1):- S1 > 0,S is S1-1,action(forward,S),orientation(O,S),position(P,S),adjacent2(O,P,Pa),asserta(position(Pa,S1)).
+est(S1):- S1 > 0,S is S1-1,action(turnright,S),orientation(O,S),moveRight(O,O1),asserta(orientation(O1,S1)).
+est(S1):- S1 > 0,S is S1-1,action(turnleft,S),orientation(O,S),moveLeft(O,O1),asserta(orientation(O1,S1)).
+
+est(S1):- S1 > 0,S is S1-1,stench(P,S),asserta(stench(P,S1)).
+est(S1):- S1 > 0,S is S1-1,breeze(P,S),asserta(breeze(P,S1)).
+est(S1):- S1 > 0,S is S1-1,glitter(P,S),asserta(glitter(P,S1)).
+est(S1):- S1 > 0,S is S1-1,bump(P,S),asserta(bump(P,S1)).
+est(S1):- S1 > 0,S is S1-1,wumpusKilled(S),asserta(wumpusKilled(S1)).
+est(S1):- S1 > 0,S is S1-1,at(nothing,P,S),asserta(at(nothing,P,S1)).
+
+est(S1):- S1 > 0,S is S1-1,action(grab,S),asserta(holding(gold,S1)).
+est(S1):- S1 > 0,S is S1-1,action(A,S),holding(gold,S),A\=release,asserta(holding(gold,S1)).
+
+est(S1):- S1 > 0,S is S1-1,action(A,S),A\=shoot,asserta(holding(arrow,S1)).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Actions Evaluations                                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+excelent(grab,S) :- position(P,S),glitter(P,S).
+excelent(shoot,S) :- holding(arrow,S),orientation(O,S),position(P,S),adjacent2(O,P,Pa),at(wumpus,Pa,S).
+
+veryGood(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),safe(Pa,S),unknown(Pa,S).
+veryGood(turnright,S) :- orientation(O,S),position(P,S),moveRight(O,O1),adjacent2(O1,P,Pa),safe(Pa,S),unknown(Pa,S).
+veryGood(turnleft,S) :- orientation(O,S),position(P,S),moveLeft(O,O1),adjacent2(O1,P,Pa),safe(Pa,S),unknown(Pa,S).
+
+good(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),safe(Pa,S),adjacent(Pa,Ps),unknown(Ps,S).
+good(turnright,S) :- position(P,S),adjacent(P,Pa),safe(Pa,S),adjacent(Pa,Ps),unknown(Ps,S).
+
+notSoGood(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),unknown(Pa,S).
+notSoGood(turnright,S) :- position(P,S),adjacent(P,Pa),unknown(Pa,S).
+
+bad(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),belief(wumpus,Pa,S).
+bad(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),belief(pit,Pa,S).
+
+% bestAction:
+bestAction(noAction,S) :- holding(gold,S).
+bestAction(A,S) :- excelent(A,S).
+bestAction(A,S) :- veryGood(A,S).
+bestAction(A,S) :- good(A,S).
+bestAction(A,S) :- notSoGood(A,S).
+bestAction(A,S) :- bad(A,S).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Extra sentences                                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 maxCell(S) :- S is 3.
 
@@ -33,164 +196,4 @@ moveLeft(right,up).
 moveLeft(down,right).
 moveLeft(left,down).
 moveLeft(up,left).
-
-count(Template, Query, Count) :-
-	setof(Template, Query, Set),
-	length(Set, Count).
-
-count(Query, Count) :-
-	count(Query, Query, Count).
-
-% Situation must be initialized to 0.
-actualSituation(0).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Diagnostic rules                                %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-stench(P,S) :-
-	perception([stench,_,_,_,_]),
-	position(P,S).
-
-breeze(P,S) :-
-	perception([_,breeze,_,_,_]),
-	position(P,S).
-
-glitter(P,S) :-
-	perception([_,_,glitter,_,_]),
-	position(P,S).
-
-bump(S) :-
-	perception([_,_,_,bump,_]).
-
-wumpusKilled(S) :-
-	perception([_,_,_,_,wumpusScream]).
-
-at(nothing,P,S) :-
-	perception([nothing,nothing,nothing,nothing,nothing]),
-	position(P,S).
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Causal rules                                    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% holding(O,S) : where O is an object holded by the agent, and S is a
-% situation.
-
-% action(A,S) : where A is an action taken by the agent at situation S.
-
-% at(O,P,S) : where O is an object, P is a location, and S is a situation.
-
-% belief(O,P,S) : where O is an object that the agent *believes* is at location
-% P in situaion S.
-
-safe(P,S) :-
-	at(nothing,P,S).
-
-safe(Pa,S) :-
-	at(nothing,P,S),
-	adjacent(P,Pa),
-	not(at(wall,Pa,S)).
-
-unknown([R,C],S) :-
-	R < 4, C < 4,
-	not(stench([R,C],S)),
-	not(breeze([R,C],S)),
-	not(glitter([R,C],S)),
-	not(at(Sm,[R,C],S)).
-
-belief(wumpus,Pa,S) :-
-	stench(P,S),
-	adjacent(P,Pa),
-	not(safe(Pa,S)).
-%	count((adjacent(Pa,Pb),(unknown(Pb);stench(Pb))),4).
-
-%at(wumpus,P) :-
-%	belief(wumpus,P),
-%	count((belief(wumpus,P)),1).
-
-%stench(P) :- at(wumpus,P).
-%stench(Pa) :- at(wumpus,P),adjacent(P,Pa).
-
-belief(pit,Pa,S) :-
-	breeze(P,S),
-	adjacent(P,Pa),
-	not(safe(Pa,S)).
-%	count((adjacent(Pa,Pb),(unknown(Pb);breeze(Pb))),4).
-
-%at(pit,P) :-
-%	belief(pit,P),
-%	count((belief(pit,P)),1).
-
-at(wall,Pa,S) :-
-	bump(S),
-	position(P,S),
-	orientation(O,S),
-	adjacent2(O,P,Pa).
-
-existUnknownCell(S) :-
-	rows(R),cols(C),
-	unknown([R,C],S),!.
-
-% present: O is an object present in the current room, and S is a situation.
-%present(O,S).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Successor-State Axioms                          %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% When position doesn't change.
-est(S1):- S1 > 0,S is S1-1,action(A,S),A\=forward,position(P,S),asserta(position(P,S1)).
-est(S1):- S1 > 0,S is S1-1,action(forward,S),position(P,S),orientation(O,S),not(adjacent2(O,P,_)),asserta(position(P,S1)).
-
-% When orientation doesn't change.
-est(S1):- S1 > 0,S is S1-1,action(A,S),A\=turnright,A\=turnleft,orientation(O,S),asserta(orientation(O,S1)).
-
-% Position or orientation change.
-est(S1):- S1 > 0,S is S1-1,action(forward,S),orientation(O,S),position(P,S),adjacent2(O,P,Pa),asserta(position(Pa,S1)).
-est(S1):- S1 > 0,S is S1-1,action(turnright,S),orientation(O,S),moveRight(O,O1),asserta(orientation(O1,S1)).
-est(S1):- S1 > 0,S is S1-1,action(turnleft,S),orientation(O,S),moveLeft(O,O1),asserta(orientation(O1,S1)).
-
-est(S1):- S1 > 0,S is S1-1,stench(P,S),asserta(stench(P,S1)).
-est(S1):- S1 > 0,S is S1-1,breeze(P,S),asserta(breeze(P,S1)).
-est(S1):- S1 > 0,S is S1-1,glitter(P,S),asserta(glitter(P,S1)).
-est(S1):- S1 > 0,S is S1-1,bump(P,S),asserta(bump(P,S1)).
-est(S1):- S1 > 0,S is S1-1,wumpusKilled(S),asserta(wumpusKilled(S1)).
-est(S1):- S1 > 0,S is S1-1,at(nothing,P,S),asserta(at(nothing,P,S1)).
-
-est(S1):- S1 > 0,S is S1-1,action(grab,S),asserta(holding(gold,S1)).
-est(S1):- S1 > 0,S is S1-1,action(A,S),holding(gold,S),A\=release,asserta(holding(gold,S1)).
-
-est(S1):- S1 > 0,S is S1-1,action(A,S),A\=shoot,asserta(holding(arrow,S1)).
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Actions                                         %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-excelent(grab,S) :- position(P,S),glitter(P,S).
-excelent(shoot,S) :- holding(arrow,S),orientation(O,S),position(P,S),adjacent2(O,P,Pa),at(wumpus,Pa,S).
-
-veryGood(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),safe(Pa,S),unknown(Pa,S).
-veryGood(turnright,S) :- orientation(O,S),position(P,S),moveRight(O,O1),adjacent2(O1,P,Pa),safe(Pa,S),unknown(Pa,S).
-veryGood(turnleft,S) :- orientation(O,S),position(P,S),moveLeft(O,O1),adjacent2(O1,P,Pa),safe(Pa,S),unknown(Pa,S).
-
-good(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),safe(Pa,S),adjacent(Pa,Ps),unknown(Ps,S).
-good(turnright,S) :- position(P,S),adjacent(P,Pa),safe(Pa,S),adjacent(Pa,Ps),unknown(Ps,S).
-
-notSoGood(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),unknown(Pa,S).
-notSoGood(turnright,S) :- position(P,S),adjacent(P,Pa),unknown(Pa,S).
-
-bad(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),belief(wumpus,Pa,S).
-bad(forward,S) :- orientation(O,S),position(P,S),adjacent2(O,P,Pa),belief(pit,Pa,S).
-
-% bestAction:
-bestAction(noAction,S) :- holding(gold,S).
-bestAction(A,S) :- excelent(A,S).
-bestAction(A,S) :- veryGood(A,S).
-bestAction(A,S) :- good(A,S).
-bestAction(A,S) :- notSoGood(A,S).
-bestAction(A,S) :- bad(A,S).
 
